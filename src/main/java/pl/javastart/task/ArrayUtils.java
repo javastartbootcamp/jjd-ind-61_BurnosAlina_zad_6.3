b9a3df5d456e0541926f62da5a333edcd80ef554
Alina Burnos <burnosalina@gmail.com>
2023-01-31 08:26:45 +0100

    Wczytywanie rozmiaru tablicy wielowymiarowej i obliczanie wartości - poprawka

--- src/main/java/pl/javastart/task/ArrayUtils.java
+++ src/main/java/pl/javastart/task/ArrayUtils.java
@@ -5,26 +5,23 @@ import java.util.Scanner;
 public class ArrayUtils {
 
     private Scanner scanner = new Scanner(System.in);
-    private int row;
-    private int columns;
-    private int[][] array;
 
-    void getRowAndColumns() {
+    int getNumber() {
+        int number;
         do {
-            System.out.println("Podaj liczbę wierszy (max 100)");
-            row = scanner.nextInt();
-            System.out.println("Podaj liczbę kolumn (max 100)");
-            columns = scanner.nextInt();
-        } while (!((row > 0 && row <= 100) && (columns > 0 && columns <= 100)));
+            number = scanner.nextInt();
+        } while (!((number > 0 && number <= 100)));
+        return number;
     }
 
-    void createArray() {
-        array = new int[row][columns];
+    int[][] createArray(int row, int columns) {
+        int[][] array = new int[row][columns];
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
                 array[i][j] = i * j;
             }
         }
+        return array;
     }
 
     void printArray(int[][] array) {
@@ -32,16 +29,8 @@ public class ArrayUtils {
             for (int columns : row) {
                 System.out.printf("%-4d |", columns);
             }
-            System.out.printf("%n%s%n", "------".repeat(columns));
+            System.out.printf("%n%s%n", "------".repeat(array[0].length));
         }
-    }
-
-    int[][] getArray() {
-        return array;
-    }
-
-    void setArray(int[][] array) {
-        this.array = array;
     }
 }
 
--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -4,8 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         ArrayUtils utils = new ArrayUtils();
-        utils.getRowAndColumns();
-        utils.createArray();
-        utils.printArray(utils.getArray());
+        System.out.println("Podaj liczbę wierszy (max 100)");
+        int row = utils.getNumber();
+        System.out.println("Podaj liczbę kolumn (max 100)");
+        int columns = utils.getNumber();
+        int[][] array = utils.createArray(row, columns);
+        utils.printArray(array);
     }
 }
